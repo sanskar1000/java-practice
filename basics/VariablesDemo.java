9be3acbe921900c252ce5f12a41a50f166c8cec5
sanskar1000 <aradhy122@gmail.com>
2026-01-19 16:46:38 +0530

    Add VariablesDemo.java

---
+++ basics/VariablesDemo.java
@@ -0,0 +1,8 @@
+class VariablesDemo {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        System.out.println("Sum = " + sum);
+    }
+}
