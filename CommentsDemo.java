d9c499bee19325e47bc3e6a51ca5859ad4fcba0f
sanskar1000 <aradhy122@gmail.com>
2026-01-21 22:22:50 +0530

    Add CommentsDemo program CommentsDemo.java

---
+++ CommentsDemo.java
@@ -0,0 +1,15 @@
+package basics;
+
+public class CommentsDemo {
+    public static void main(String[] args) {
+
+        // This is a single-line comment
+
+        /*
+         This is a
+         multi-line comment
+        */
+
+        System.out.println("Comments demo");
+    }
+}
