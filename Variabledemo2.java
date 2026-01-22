744ebdc327b8eeff1ab8b7518eebeb6447103122
sanskar1000 <aradhy122@gmail.com>
2026-01-22 22:15:44 +0530

    add second program using v Variabledemo2.java

---
+++ Variabledemo2.java
@@ -0,0 +1,11 @@
+package basics;
+
+public class Main {
+    public static void main(String[] args) {
+        // Type your code below
+        String result = "Hii there";
+        
+        // Don't change the line below
+        System.out.println("Massage  = \"" + Result + "\"");
+    }
+}
