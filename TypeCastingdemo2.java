0fe65f57d63b96601fa8fe9a2eac0cf3446a2cf4
sanskar1000 <aradhy122@gmail.com>
2026-01-23 16:41:11 +0530

    Add another program typecasting TypeCastingdemo2.java

---
+++ TypeCastingdemo2.java
@@ -0,0 +1,15 @@
+package basics;
+
+public class Main {
+    public static void main(String[] args) {
+        // Declare and initialize variables
+        double price = 99.99;
+        int intPrice = (int) price;
+        
+        
+        // Output the values
+        System.out.println("Price: " + price);
+        System.out.println("Int Price: " + intPrice);
+        
+    }
+}
