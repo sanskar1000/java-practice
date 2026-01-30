e4cf0574f4dc3066d82c79c7e9d8117b747077e9
sanskar1000 <aradhy122@gmail.com>
2026-01-30 21:18:11 +0530

    Add else-if ladder example for grading system

---
+++ ElseIfLadderDemo.java
@@ -0,0 +1,32 @@
+//Demonstrates else-if ladder
+package condition; 
+
+import java.util.Scanner;
+
+public class ElseIfLadderDemo {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter marks: ");
+        int marks = sc.nextInt();
+
+        if (marks >= 90) {
+            System.out.println("Grade: A");
+        } 
+        else if (marks >= 75) {
+            System.out.println("Grade: B");
+        } 
+        else if (marks >= 60) {
+            System.out.println("Grade: C");
+        } 
+        else if (marks >= 40) {
+            System.out.println("Grade: D");
+        } 
+        else {
+            System.out.println("Grade: Fail");
+        }
+
+    }
+}
