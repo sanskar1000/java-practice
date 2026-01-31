fde3196ac3ed87435bdf080ff69fa0560a10301d
sanskar1000 <aradhy122@gmail.com>
2026-01-31 21:48:09 +0530

    Add class name main to positive negative

--- condition/practice/PositiveNegative.java
+++ condition/practice/PositiveNegative.java
@@ -3,7 +3,7 @@ package condition.practice;
 
 import java.util.Scanner;
 
-public class Main {
+public class PositveNegative {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
