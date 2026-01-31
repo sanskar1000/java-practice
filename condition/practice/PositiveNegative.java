06d258f7b2b7f60c6ed535d469ecea4cb154eed8
sanskar1000 <aradhy122@gmail.com>
2026-01-31 21:32:38 +0530

    Add if-else practice programs under condition package

---
+++ condition/practice/PositiveNegative.java
@@ -0,0 +1,20 @@
+// Practice: check positive and negative number
+package condition.practice;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number :");
+        int num = sc.nextInt();
+
+        if ( num >= 0){
+            System.out.println("The number is Positive. ");
+        }else {
+            System.out.println("The number is Negative. ");
+         }
+     }
+ }
