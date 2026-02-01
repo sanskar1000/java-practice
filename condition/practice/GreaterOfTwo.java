58a2d5df4f72d87bf88cd4ca58c5dedaca9776bc
sanskar1000 <aradhy122@gmail.com>
2026-02-01 21:45:51 +0530

    Add if-else practice programs under condition package

---
+++ condition/practice/GreaterOfTwo.java
@@ -0,0 +1,25 @@
+//Practice : check which number is greater
+package condition.practice;
+
+import java.util.Scanner;
+
+public class GreaterOfTwo  {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number : ");
+        int b = sc.nextInt();
+
+        if ( a > b){
+            System.out.println("Greater number is : "+a);
+        }else if (b > a){
+            System.out.println("Greater number is: "+b);
+        }else {
+            System.out.println("Both  number is equal.");
+       }
+     }
+   }
