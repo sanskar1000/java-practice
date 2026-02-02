af5068542c71634daae4e25d6fcd02ee6ddf943f
sanskar1000 <aradhy122@gmail.com>
2026-02-02 21:22:23 +0530

    Add while loop example program

---
+++ loops/WhileLoopDemo.java
@@ -0,0 +1,13 @@
+// Demonstrates while loop
+package loops;
+
+public class WhileLoopDemo {
+    public static void main(String[] args) {
+     
+        int i = 0;
+        while(i<6){
+        System.out.println(i);
+        i++;
+        }
+    }
+}
