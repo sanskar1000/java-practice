4e15b61aa9b47f8172aae82e37e098c186abffa4
sanskar1000 <aradhy122@gmail.com>
2026-02-02 21:28:59 +0530

    Add do while example program

---
+++ loops/DoWhileLoopDemo.java
@@ -0,0 +1,14 @@
+// Demonstrates basic do while loop
+package loops;
+
+public class DoWhileLoopDemo {
+    public static void main(String[] args) {
+     
+      int i=0;
+      do{
+        System.out.println(i);
+        i++;
+        }while(i<5);
+      
+    }
+}
