7a6769de6ad3fdb2751b5e6fc0d2c04ffd78bd22
sanskar1000 <aradhy122@gmail.com>
2026-02-05 13:49:55 +0530

    Add break  example

---
+++ control-statements-advanced/BreakExample.java
@@ -0,0 +1,17 @@
+//Demonstrates control-statments
+package controlstatementsadvanced;
+
+public class BreakExample {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+
+            if (i == 5) {
+                break;   // loop stops when i becomes 5
+            }
+
+            System.out.println(i);
+        }
+    }
+}
