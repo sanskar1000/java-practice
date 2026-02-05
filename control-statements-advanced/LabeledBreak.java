8e4dffbddec597158c0c0f0eeef091bf6d6a9ccb
sanskar1000 <aradhy122@gmail.com>
2026-02-05 16:11:09 +0530

    Add labeled break example

---
+++ control-statements-advanced/LabeledBreak.java
@@ -0,0 +1,21 @@
+//Demonstrates control-statments
+package controlstatementsadvanced;
+
+public class LabeledBreak {
+
+    public static void main(String[] args) {
+
+        outerLoop:
+        for (int i = 1; i <= 3; i++) {
+
+            for (int j = 1; j <= 3; j++) {
+
+                if (i == 2 && j == 2) {
+                    break outerLoop;   // exits both loops
+                }
+
+                System.out.println("i = " + i + ", j = " + j);
+            }
+        }
+    }
+}
