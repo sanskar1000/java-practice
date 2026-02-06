1c2a212cd4fd1b1ad8cdbf90437bd5987b831d12
Aradhya Singh <aradhy122@gmail.com>
2026-02-06 11:31:31 +0530

    Add method basics example

---
+++ methods/MethodBasics.java
@@ -0,0 +1,13 @@
+//Demonstrates methods
+package methods;
+
+public class MethodBasics {
+
+    static void greet() {
+        System.out.println("Hello, welcome to Java methods!");
+    }
+
+    public static void main(String[] args) {
+        greet();   // method call
+    }
+}
