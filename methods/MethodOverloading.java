88345041529c4b4d1c982ea779568f0ffeb7f51f
Aradhya Singh <aradhy122@gmail.com>
2026-02-08 11:13:19 +0530

    Add simple example of method overloading

---
+++ methods/MethodOverloading.java
@@ -0,0 +1,18 @@
+//Demonstrates method overloading
+package methods;
+
+public class MethodOverloading {
+
+    static int add(int a, int b) {
+        return a + b;
+    }
+
+    static int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(add(5, 10));
+        System.out.println(add(5, 10, 15));
+    }
+}
