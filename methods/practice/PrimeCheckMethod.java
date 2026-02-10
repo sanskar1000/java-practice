08bef3e1312c2267e86c237b279bf4ca5ac12647
Aradhya Singh <aradhy122@gmail.com>
2026-02-10 11:30:04 +0530

    Add method practice program

---
+++ methods/practice/PrimeCheckMethod.java
@@ -0,0 +1,20 @@
+// Practice : Check number is prime or not 
+package methods.practice;
+
+public class PrimeCheckMethod {
+
+     static boolean isPrime(int n) {
+        if (n <= 1) return false;
+
+        for (int i = 2; i <= n / 2; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPrime(11));
+    }
+}
