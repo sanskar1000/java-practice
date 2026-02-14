a9836cae9494d77d02ce1322a2823dfd16061234
Aradhya Singh <aradhy122@gmail.com>
2026-02-14 23:04:31 +0530

    Start Arrays topic with basic programs

---
+++ arrays/ArrayBasics.java
@@ -0,0 +1,15 @@
+//Demonstrate: Arraya in java with basic examlpe.
+package arrays;
+
+public class ArrayBasics {
+    public static void main(String[] args) {
+
+        int[] arr = {10, 20, 30, 40, 50};
+
+        System.out.println("Array elements:");
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
