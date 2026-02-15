04973979fd41c14d3f7ae0e868de384c346fc64e
Aradhya Singh <aradhy122@gmail.com>
2026-02-15 14:42:10 +0530

    Add example input output of array

---
+++ arrays/ArrayInputOutput.java
@@ -0,0 +1,26 @@
+//Demonstrates : Input Output in array
+package arrays;
+
+import java.util.Scanner;
+
+public class ArrayInputOutput {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter element " + i + ": ");
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Array elements:");
+        for (int i=0 ;i<arr.length;i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
